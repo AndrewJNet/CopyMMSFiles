3d98fa5b26a26dfb919bea7c796dadda32f3fa0d
forevanyeung <forevanyeung@users.noreply.github.com>
2017-05-19 09:37:16 -0500

    Added error checking for failed login
    Added error checking if user did not log in properly. Also, cleaned up folder names a bit, some folders will still have a trailing "_" that is due to sanitizing special characters.

--- CopyMMS2017Files.ps1
+++ CopyMMS2017Files.ps1
@@ -1,4 +1,4 @@
-﻿##############################################
+##############################################
 #                                            #
 # File:     Copymms2017Files.ps1             #
 # Author:   Duncan Russell                   #
@@ -19,42 +19,48 @@ $c = $host.UI.PromptForCredential('Sched Credentials', 'Enter Credentials', '', 
 $form = $web.Forms[1]
 $form.fields['username'] = $c.UserName
 $form.fields['password'] = $c.GetNetworkCredential().Password
-"Logging In..."
-$web = Invoke-WebRequest -Uri 'https://mms2017.sched.com/login' -WebSession $mms -Method POST -Body $form.Fields
+"Loggin in..."
+$web = Invoke-WebRequest 'https://mms2017.sched.com/login' -WebSession $mms -Method POST -Body $form.Fields
+if(-Not ($web.InputFields.FindByName("login"))) {
 
-ForEach ($Date in $MMSDates) {
-    "Checking day '{0}' for downloads" -f $Date
-    
-    $sched = Invoke-WebRequest -Uri $("https://mms2017.sched.org/" + $Date + "/list/descriptions") -WebSession $mms
-    $links = $sched.Links
+    ForEach ($Date in $MMSDates) {
+        "Checking day '{0}' for downloads" -f $Date
 
-    $eventsIndex = @()
-    $links | ForEach-Object { if(($_.href -like "*/event/*") -and ($_.innerText -notlike "here")) { 
-        $eventsIndex += (, ($links.IndexOf($_), $_.innerText))
-    } }
+        $sched = Invoke-WebRequest -Uri $("https://mms2017.sched.org/" + $Date + "/list/descriptions") -WebSession $mms
+        $links = $sched.Links
 
-    $i = 0
-    While($i -lt $eventsIndex.Count) {
-        $eventTitle = $eventsIndex[$i][1]
-        $eventTitle = $eventTitle -replace "\W+", "_"
+        $eventsIndex = @()
+        $links | ForEach-Object { if(($_.href -like "*/event/*") -and ($_.innerText -notlike "here")) { 
+            $eventsIndex += (, ($links.IndexOf($_), $_.innerText))
+        } }
 
-        $links[$eventsIndex[$i][0]..$(if($i -eq $eventsIndex.Count - 1) {$links.Count-1} else {$eventsIndex[$i+1][0]})] | ForEach-Object { 
-            if($_.href -like "*hosted_files*") { 
-                $downloadPath = $baseLocation + '\mms2017\' + $Date + '\' + $eventTitle
-                $filename = $_.href
-                $filename = $filename.substring(39)
-                $filename = $filename.Replace('%20',' ')
-                $outputFilePath = $downloadPath + '\' + $filename
-                if((Test-Path -Path $($downloadPath)) -eq $false) { New-Item -ItemType Directory -Force -Path $downloadPath | Out-Null }
-                if((Test-Path -Path $outputFilePath) -eq $false)
-                {
-                    "...attempting to download '{0}'" -f $filename
-                    Invoke-WebRequest -Uri $_.href -OutFile $outputfilepath -WebSession $mms
-                    Unblock-File $outputFilePath
-                    $stopit = $true
-                }
-            } 
+        $i = 0
+        While($i -lt $eventsIndex.Count) {
+            $eventTitle = $eventsIndex[$i][1]
+            $eventTitle = $eventTitle.Trim()
+            $eventTitle = $eventTitle -replace "\W+", "_"
+
+            $links[$eventsIndex[$i][0]..$(if($i -eq $eventsIndex.Count - 1) {$links.Count-1} else {$eventsIndex[$i+1][0]})] | ForEach-Object { 
+                if($_.href -like "*hosted_files*") { 
+                    $downloadPath = $baseLocation + '\mms2017\' + $Date + '\' + $eventTitle
+                    $filename = $_.href
+                    $filename = $filename.substring(39)
+                    $filename = $filename.Replace('%20',' ')
+                    $outputFilePath = $downloadPath + '\' + $filename
+                    if((Test-Path -Path $($downloadPath)) -eq $false) { New-Item -ItemType Directory -Force -Path $downloadPath | Out-Null }
+                    if((Test-Path -Path $outputFilePath) -eq $false)
+                    {
+                        "...attempting to download '{0}'" -f $filename
+                        Invoke-WebRequest -Uri $_.href -OutFile $outputfilepath -WebSession $mms
+                        Unblock-File $outputFilePath
+                        $stopit = $true
+                    }
+                } 
+            }
+
+            $i++
         }
-        $i++
     }
+} else {
+    "Login failed. Exiting script."
 }
