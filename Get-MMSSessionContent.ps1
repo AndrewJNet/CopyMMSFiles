bd5522aaa289b71442570cf33ac37ac70def1728
Andrew Johnson <15828090+AndrewJNet@users.noreply.github.com>
2023-11-01 15:47:51 -0500

    Update Get-MMSSessionContent.ps1
    Updated for MMS Miami 2023

--- Get-MMSSessionContent.ps1
+++ Get-MMSSessionContent.ps1
@@ -61,7 +61,7 @@
 Param(
   [Parameter(Mandatory = $false)][string]$DownloadLocation = "C:\Conferences\MMS", # could validate this: [ValidateScript({(Test-Path -Path (Split-Path $PSItem))})]
   [Parameter(Mandatory = $true, ParameterSetName = 'SingleEvent')]
-  [ValidateSet("2015", "2016", "2017", "2018", "de2018", "2019", "jazz", "miami", "2022atmoa", "2023atmoa")]
+  [ValidateSet("2015", "2016", "2017", "2018", "de2018", "2019", "jazz", "miami", "2022atmoa", "2023atmoa","2023miami")]
   [string]$ConferenceId,
   [Parameter(Mandatory = $true, ParameterSetName = 'MultipleEvents', HelpMessage = "This needs to bwe a list or array of conference ids/years!")]
   [System.Collections.Generic.List[string]]$ConferenceList,
@@ -74,7 +74,7 @@ $DownloadLocation = $DownloadLocation.Trim('\')
 
 ## Setup
 $PublicContentYears = @('2015', '2016', '2017')
-$PrivateContentYears = @('2018', 'de2018', '2019', 'jazz', 'miami', '2022atmoa', '2023atmoa')
+$PrivateContentYears = @('2018', 'de2018', '2019', 'jazz', 'miami', '2022atmoa', '2023atmoa','2023miami')
 $ConferenceYears = New-Object -TypeName System.Collections.Generic.List[string]
 [int]$PublicYearsCount = $PublicContentYears.Count
 [int]$PrivateYearsCount = $PrivateContentYears.Count
